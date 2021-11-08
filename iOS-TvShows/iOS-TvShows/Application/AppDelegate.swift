80d31f3dd00e9f30964b49813d03f8ce7a4b16ee
BanFilip <filip.ban@infinum.com>
2021-11-08 12:16:17 +0100

    Added Login module

--- iOS-TvShows/iOS-TvShows/Application/AppDelegate.swift
+++ iOS-TvShows/iOS-TvShows/Application/AppDelegate.swift
@@ -13,13 +13,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let initialViewController = UINavigationController()
 
         window = UIWindow(frame: UIScreen.main.bounds)
         guard let window = window else {
             return false
         }
 
-        window.rootViewController = ViewController()
+        initialViewController.setRootWireframe(LoginWireframe())
+        window.rootViewController = initialViewController
         window.makeKeyAndVisible()
         return true
     }
